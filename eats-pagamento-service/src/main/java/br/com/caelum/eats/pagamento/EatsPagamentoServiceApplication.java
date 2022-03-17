2f959a47a4754e32464f15089013ec27e830d0ab
Anderson <andersondomanowski@hotmail.com>
2022-03-17 19:11:02 -0300

    Hytrix e Threads

--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/EatsPagamentoServiceApplication.java
@@ -2,11 +2,13 @@ package br.com.caelum.eats.pagamento;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
+@EnableScheduling
 @SpringBootApplication
 public class EatsPagamentoServiceApplication {
 
--- eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
+++ eats-pagamento-service/src/main/java/br/com/caelum/eats/pagamento/PagamentoController.java
@@ -1,13 +1,24 @@
 package br.com.caelum.eats.pagamento;
 
-import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.util.UriComponentsBuilder;
-
 import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import br.com.caelum.eats.pagamento.Pagamento.Status;
+import lombok.AllArgsConstructor;
 
 @RestController
 @RequestMapping("/pagamentos")
@@ -26,6 +37,7 @@ class PagamentoController {
 	}
 
 	@GetMapping("/{id}")
+	@HystrixCommand(threadPoolKey = "getByIdPgtoThreadPool")
 	PagamentoDto detalha(@PathVariable("id") Long id) {
 		return pagamentoRepo.findById(id)
 				.map(PagamentoDto::new)
@@ -41,10 +53,13 @@ class PagamentoController {
 	}
 
 	@PutMapping("/{id}")
+	@HystrixCommand(fallbackMethod = "realizaPagamentoFallback",
+	threadPoolKey = "putPagamentoThreadPool")
 	PagamentoDto confirma(@PathVariable("id") Long id) {
 		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
 		pagamento.setStatus(Pagamento.Status.CONFIRMADO);
 		pedidoCliente.notificaServicoDePedidoParaMudarStatus(pagamento.getPedidoId(), new MudancaDeStatusDoPedido("pago"));
+		pagamento.setStatus(Status.CONFIRMADO);
 		pagamentoRepo.save(pagamento);
 		return new PagamentoDto(pagamento);
 	}
@@ -53,6 +68,13 @@ class PagamentoController {
 	PagamentoDto cancela(@PathVariable("id") Long id) {
 		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
 		pagamento.setStatus(Pagamento.Status.CANCELADO);
+		pagamentoRepo.save(pagamento);
+		return new PagamentoDto(pagamento);
+	}
+	
+	public PagamentoDto realizaPagamentoFallback(Long id) {
+		Pagamento pagamento = pagamentoRepo.findById(id).orElseThrow(ResourceNotFoundException::new);
+		pagamento.setStatus(Status.PROCESSADO);
 		pagamentoRepo.save(pagamento);
 		return new PagamentoDto(pagamento);
 	}
